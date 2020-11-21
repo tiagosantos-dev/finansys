4b35ded1e00e3e0ae2735f8a5183616916c6d027
tiagosantos-dev <tiagoltj@hotmail.com>
2020-11-21 14:22:40 -0300

    adding entry model and service

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,29 @@
+import { Category } from '../../categories/shared/category.model';
+
+export class Entry{
+    constructor(
+        public id? :number,
+        public name? :string,
+        public description? :string,
+        public type ?:string,
+        public amount? :string,
+        public date? :string,
+        public paid? :boolean,
+        public category_id? :number,
+        public category? :Category,
+        ){}
+    
+    get paidText() :string{
+        return this.paid ? 'Pago' :'Pendente'
+    
+    }    
+
+}
+
+
+export enum Types {
+    expense = "Despesa",
+    renevue = "Receita"
+}
+
+
---
+++ src/app/pages/entries/shared/entry.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, flatMap } from 'rxjs/operators';
+
+import { Entry } from './entry.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class EntryService {
+
+  private apiPath :string = "http://localhost:3000/categories";
+
+  constructor(private http: HttpClient ) { }
+
+  public getAll() :Observable<Entry[]>{
+    return this.http.get(this.apiPath).pipe(catchError(this.handlerError))
+
+  }
+
+  private handlerError(error : any):Observable<any>{
+    console.log("Error na requisição", error)
+    return throwError(error) 
+  }
+
+  private jsonDataEntries(jsonData: any):Entry[]{
+    const entries = [];
+    jsonData.forEach(element => {
+        entries.push(element as Entry)
+    });
+    return entries;
+  }
+
+  public getById(id : number): Observable<Entry>{
+    const URL = `${this.apiPath}/${id}`
+    return this.http.get(URL).pipe(catchError(this.handlerError))
+
+  }
+
+  public create(entry: Entry) : Observable<Entry>{
+    return this.http.post(this.apiPath, entry).pipe(catchError(this.handlerError))
+
+  }
+
+  public update(entry : Entry) {
+    const URL = `http://localhost:3000/categories/${entry.id}`;
+    return this.http.patch(URL, entry).pipe(catchError(this.handlerError))
+
+  }
+
+  public delete(id :number) : Observable<any>{
+    return this.http.delete(`${this.apiPath}/${id}`).pipe(catchError(this.handlerError), map(()=> id))
+
+  }
+
+}
